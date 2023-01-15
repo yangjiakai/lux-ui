7977bbb1a4f91171ac5ced893d76460907f08c62
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-01-16 02:08:13 +0900

    全局主题切换完成

--- src/components/CustomizationMenu.vue
+++ src/components/CustomizationMenu.vue
@@ -1,61 +1,7 @@
-<template>
-  <div>
-    <div class="drawer-button" @click="themeDrawer = true">
-      <v-icon class="text-white">mdi-cog-outline</v-icon>
-    </div>
-
-    <v-navigation-drawer
-      v-model="themeDrawer"
-      location="right"
-      temporary
-      width="300"
-      color="rgba(255,255,255,.7)"
-    >
-      <div class="d-flex align-center pa-2">
-        <div class="text-body-2">Settings</div>
-        <v-spacer></v-spacer>
-        <v-btn
-          variant="text"
-          color="primary"
-          size="small"
-          rounded
-          icon="mdi-close"
-          @click="themeDrawer = false"
-        >
-        </v-btn>
-      </div>
-      <v-divider></v-divider>
-
-      <v-switch
-        v-model="customizeTheme.darkTheme"
-        hide-details
-        :label="`Theme: ${currentTheme}`"
-        inset
-      ></v-switch>
-      <v-divider></v-divider>
-      <div class="pa-2">
-        <div class="font-weight-bold my-1">Global Theme</div>
-        <v-btn-toggle v-model="theme" color="primary" mandatory class="mb-2">
-          <v-btn x-large>Light</v-btn>
-          <v-btn x-large>Dark</v-btn>
-        </v-btn-toggle>
-        <v-divider></v-divider>
-        <v-color-picker
-          v-model="color"
-          mode="hexa"
-          :swatches="swatches"
-          show-swatches
-        ></v-color-picker>
-        <v-btn color="primary">text</v-btn>
-        <h1 class="text-primary">3333</h1>
-      </div>
-    </v-navigation-drawer>
-  </div>
-</template>
-
 <script setup lang="ts">
 import { useTheme } from "vuetify";
 import { useCustomizeThemeStore } from "@/stores/customizeTheme";
+import { Icon } from "@iconify/vue";
 const customizeTheme = useCustomizeThemeStore();
 const theme = useTheme();
 
@@ -77,6 +23,89 @@ watch(color, (newVal) => {
   console.log(theme.current.value.colors.primary);
 });
 </script>
+
+<template>
+  <div>
+    <div class="drawer-button" @click="themeDrawer = true">
+      <v-icon class="text-white">mdi-cog-outline</v-icon>
+    </div>
+
+    <v-navigation-drawer
+      v-model="themeDrawer"
+      location="right"
+      temporary
+      width="300"
+      class="theme-drawer pa-8"
+    >
+      <div class="top-area">
+        <div class="d-flex align-center">
+          <b>Soft UI Configurator</b>
+          <v-spacer></v-spacer>
+          <v-btn
+            variant="text"
+            color="primary"
+            size="small"
+            rounded
+            icon="mdi-close"
+            @click="themeDrawer = false"
+          >
+          </v-btn>
+        </div>
+        <div>See our dashboard options.</div>
+      </div>
+
+      <hr class="my-6" />
+
+      <div class="theme-area">
+        <b>Global Theme Mode</b>
+        <div class="px-3 pt-3" v-if="customizeTheme.darkTheme">
+          <v-btn
+            @click="customizeTheme.darkTheme = !customizeTheme.darkTheme"
+            icon
+            color="grey-darken-4"
+            class="text-white"
+          >
+            <Icon width="30" icon="line-md:moon-filled-loop" />
+          </v-btn>
+          <span class="ml-5">Dark Mode</span>
+        </div>
+        <div class="px-3 pt-3" v-else>
+          <v-btn
+            @click="customizeTheme.darkTheme = !customizeTheme.darkTheme"
+            icon
+            color="white"
+            class="text-red"
+          >
+            <Icon
+              width="30"
+              icon="line-md:moon-filled-alt-to-sunny-filled-loop-transition"
+            />
+          </v-btn>
+          <span class="ml-5">Light Mode</span>
+        </div>
+      </div>
+      <hr class="my-6" />
+
+      <v-divider></v-divider>
+      <div class="pa-2">
+        <div class="font-weight-bold my-1">Global Theme</div>
+        <v-btn-toggle v-model="theme" color="primary" mandatory class="mb-2">
+          <v-btn x-large>Light</v-btn>
+          <v-btn x-large>Dark</v-btn>
+        </v-btn-toggle>
+        <v-divider></v-divider>
+        <v-color-picker
+          v-model="color"
+          mode="hexa"
+          :swatches="swatches"
+          show-swatches
+        ></v-color-picker>
+        <v-btn color="primary">text</v-btn>
+        <h1 class="text-primary">3333</h1>
+      </div>
+    </v-navigation-drawer>
+  </div>
+</template>
 
 <style lang="scss" scoped>
 .drawer-button {
@@ -109,5 +138,18 @@ watch(color, (newVal) => {
       transform: rotate(360deg);
     }
   }
+}
+
+hr {
+  background-image: linear-gradient(
+    90deg,
+    transparent,
+    rgba(0, 0, 0, 0.4),
+    transparent
+  ) !important;
+  background-color: transparent;
+  opacity: 0.25;
+  border: none;
+  height: 1px;
 }
 </style>
--- src/views/ui/ColorsPage.vue
+++ src/views/ui/ColorsPage.vue
@@ -2,13 +2,10 @@
   <v-container>
     <Icon class="animate-icon" icon="logos:bamboo" />
     <!-- <Icon :icon="item" v-for="item in listIcons()" :key="item" /> -->
-    <copy-label text="ccccc" />
-    <p>asdasd<copy-label text="kkkk" />aaa</p>
   </v-container>
 </template>
 <script setup lang="ts">
 import { Icon, listIcons } from "@iconify/vue";
-import CopyLabel from "@/components/common/CopyLabel.vue";
 </script>
 <style scoped lang="scss">
 @keyframes rotate {
