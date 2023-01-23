7876b4b82cd5d20728d4b06675539ed2133ec239
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-01-24 01:13:22 +0900

    Tabs Create

--- src/views/app/unsplash/MasonryBackup.vue
+++ src/views/app/unsplash/MasonryBackup.vue
@@ -38,25 +38,6 @@ const searchParams = reactive({
 
 const searchResult = ref([]);
 const photos = ref([]);
-
-const photoData = reactive({
-  photos: [],
-  total: 0,
-  totalPages: 0,
-});
-
-const collectionData = reactive({
-  collections: [],
-  total: 0,
-  totalPages: 0,
-});
-
-const userData = reactive({
-  users: [],
-  total: 0,
-  totalPages: 0,
-});
-
 const collections = ref([]);
 const users = ref([]);
 const relatedSearches = ref([]);
@@ -70,25 +51,9 @@ const search = async () => {
 
   // searchResult.value = response.data.results;
   // console.log(searchResult.value);
-  // Photos
-  photoData.photos = response.data.photos.results;
-  photoData.total = response.data.photos.total;
-  photoData.totalPages = response.data.photos.total_pages;
-
-  // Collections
-  collectionData.collections = response.data.collections.results;
-  collectionData.total = response.data.collections.total;
-  collectionData.totalPages = response.data.collections.total_pages;
-
-  // Users
-  userData.users = response.data.users.results;
-  userData.total = response.data.users.total;
-  userData.totalPages = response.data.users.total_pages;
-
-  // photos.value = response.data.photos.results;
-  // collections.value = response.data.collections.results;
-  // users.value = response.data.users.results;
-  // RelatedSearches
+  photos.value = response.data.photos.results;
+  collections.value = response.data.collections.results;
+  users.value = response.data.users.results;
   relatedSearches.value = response.data.related_searches;
 };
 
@@ -101,21 +66,61 @@ const more = async () => {
   const response = await axios.get(
     `${url.value}client_id=${searchParams.accessKey}&page=${searchParams.page}&per_page=${searchParams.perPage}&query=${searchParams.query}`
   );
-  photoData.photos.push(...response.data.photos.results);
-};
-
-const morePhotos = async () => {
-  searchParams.page++;
-  const response = await axios.get(
-    `${url.value}client_id=${searchParams.accessKey}&page=${searchParams.page}&per_page=${searchParams.perPage}&query=${searchParams.query}`
-  );
-  photoData.photos.push(...response.data.photos.results);
+  searchResult.value.push(...response.data.results);
 };
 
 const searchRelated = (query: string) => {
   searchParams.query = query;
   search();
 };
+
+// const append = async () => {
+//   console.log("append");
+
+//   searchParams.page++;
+//   const response = await axios.get(
+//     `${url.value}client_id=${searchParams.accessKey}&page=${searchParams.page}&per_page=${searchParams.perPage}&query=${searchParams.query}`
+//   );
+
+//   console.log(searchResult.value);
+//   searchResult.value.push(...response.data.results);
+// };
+
+// const options = reactive({
+//   // 唯一key值
+//   rowKey: "id",
+//   // 卡片之间的间隙
+//   gutter: 10,
+//   // 是否有周围的gutter
+//   hasAroundGutter: true,
+//   // 卡片在PC上的宽度
+//   width: 200,
+
+//   // 自定义行显示个数，主要用于对移动端的适配
+//   breakpoints: {
+//     1200: {
+//       // 当屏幕宽度小于等于1200
+//       rowPerView: 4,
+//     },
+//     800: {
+//       // 当屏幕宽度小于等于800
+//       rowPerView: 3,
+//     },
+//     500: {
+//       // 当屏幕宽度小于等于500
+//       rowPerView: 2,
+//     },
+//   },
+//   // 动画效果
+//   animationEffect: "animate__fadeInUp",
+//   // 动画时间
+//   animationDuration: 1000,
+//   // 动画延迟
+//   animationDelay: 300,
+//   // 背景色
+//   backgroundColor: "#2C2E3A",
+//   // 是否懒加载
+// });
 </script>
 
 <template>
@@ -134,6 +139,114 @@ const searchRelated = (query: string) => {
       <v-btn color="">Go</v-btn>
     </v-toolbar>
     <v-card class="mt-2">
+      <!-- <div
+        v-masonry="'containerId'"
+        transition-duration="0.3s"
+        item-selector=".item"
+        class="masonryWrap"
+      >
+        <div
+          v-for="item in searchResult"
+          :key="item.id"
+          v-masonry-tile
+          class="masonryWrap_item item"
+        >
+          <v-img :src="item.urls.small" width="100%" />
+          aaa
+        </div>
+      </div> -->
+
+      <!-- <div
+        v-masonry
+        class="masonryWrap mx-auto"
+        transition-duration="0.3s"
+        item-selector=".item"
+        fitWidth
+      >
+        <div
+          v-masonry-tile
+          class="item mx-auto"
+          v-for="item in searchResult"
+          :key="item.id"
+        >
+          <v-card class="ma-3" width="400">
+            <v-img
+              class="align-end text-white"
+              :src="item.urls.small"
+              :lazy-src="item.urls.small"
+              cover
+            >
+              <template v-slot:placeholder>
+                <v-row class="fill-height ma-0" align="center" justify="center">
+                  <v-progress-circular
+                    indeterminate
+                    color="grey-lighten-5"
+                  ></v-progress-circular>
+                </v-row>
+              </template>
+              <v-card-title class="card-title">{{
+                item.user.username
+              }}</v-card-title>
+            </v-img>
+
+            <v-card-subtitle class="pt-4">
+              <div>size: height:{{ item.height }} width:{{ item.width }}</div>
+            </v-card-subtitle>
+
+            <v-card-text>
+              {{ item.alt_description }}
+
+              <div>{{ item.download_url }}</div>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn color="primary"> Share </v-btn>
+
+              <v-btn color="primary"> Explore </v-btn>
+            </v-card-actions>
+          </v-card>
+        </div>
+      </div> -->
+      <!-- <masonry-wall :items="searchResult" :ssr="{ columns: 1 }">
+        <template #default="{ item }">
+          <v-card class="ma-3" max-width="400">
+            <v-img
+              class="align-end text-white"
+              :src="item.urls.small"
+              :lazy-src="item.urls.small"
+              cover
+            >
+              <template v-slot:placeholder>
+                <v-row class="fill-height ma-0" align="center" justify="center">
+                  <v-progress-circular
+                    indeterminate
+                    color="grey-lighten-5"
+                  ></v-progress-circular>
+                </v-row>
+              </template>
+              <v-card-title class="card-title">{{
+                item.user.username
+              }}</v-card-title>
+            </v-img>
+
+            <v-card-subtitle class="pt-4">
+              <div>size: height:{{ item.height }} width:{{ item.width }}</div>
+            </v-card-subtitle>
+
+            <v-card-text>
+              {{ item.alt_description }}
+
+              <div>{{ item.download_url }}</div>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn color="primary"> Share </v-btn>
+
+              <v-btn color="primary"> Explore </v-btn>
+            </v-card-actions>
+          </v-card>
+        </template>
+      </masonry-wall> -->
       <v-tabs v-model="tab" bg-color="primary">
         <v-tab value="photos"
           ><v-icon class="mr-2">mdi-image-outline</v-icon> photos</v-tab
@@ -146,12 +259,11 @@ const searchRelated = (query: string) => {
 
       <v-card-text>
         <v-window v-model="tab">
-          <!-- Photos -->
           <v-window-item value="photos">
             <v-card
               min-height="80vh"
               class="pa-5 d-flex align-center justify-center"
-              v-if="photoData.total === 0"
+              v-if="photos.length === 0"
             >
               <v-img
                 src="https://unsplash-assets.imgix.net/empty-states/photos.png"
@@ -184,7 +296,7 @@ const searchRelated = (query: string) => {
                   lg="3"
                   md="4"
                   sm="6"
-                  v-for="item in photoData.photos"
+                  v-for="item in photos"
                   :key="item.id"
                 >
                   <v-card
@@ -253,19 +365,11 @@ const searchRelated = (query: string) => {
                   </v-card>
                 </v-col>
               </v-row>
-              <v-btn
-                v-if="searchParams.page < photoData.totalPages"
-                class="mt-10"
-                block
-                color="primary"
-                dark
-                @click="more"
+              <v-btn class="mt-10" block color="primary" dark @click="more"
                 >More...</v-btn
               >
             </v-card>
           </v-window-item>
-
-          <!-- Collections -->
           <v-window-item value="collections">
             <v-card
               min-height="80vh"
@@ -377,8 +481,6 @@ const searchRelated = (query: string) => {
               >
             </v-card>
           </v-window-item>
-
-          <!-- Users -->
           <v-window-item value="users">
             <v-card
               min-height="80vh"
@@ -492,6 +594,63 @@ const searchRelated = (query: string) => {
           </v-window-item>
         </v-window>
       </v-card-text>
+
+      <!-- <div class="masonrys">
+        <div class="aa" v-for="item in searchResult" :key="item.id">
+          <v-img :src="item.urls.small"> </v-img>
+        </div>
+      </div> -->
+      <!-- <div>
+        <h2 class="title">resposive masonry</h2>
+        <div class="masonry">
+          <v-img
+            v-for="item in searchResult"
+            :key="item.id"
+            :src="item.urls.small"
+          >
+          </v-img>
+        </div>
+      </div> -->
+      <!-- <Waterfall :list="searchResult">
+        <template #item="{ item }">
+          <v-card class="ma-3">
+            <v-img
+              class="align-end text-white"
+              :src="item.urls.small"
+              :lazy-src="item.urls.small"
+              cover
+            >
+              <template v-slot:placeholder>
+                <v-row class="fill-height ma-0" align="center" justify="center">
+                  <v-progress-circular
+                    indeterminate
+                    color="grey-lighten-5"
+                  ></v-progress-circular>
+                </v-row>
+              </template>
+              <v-card-title class="card-title">{{
+                item.user.username
+              }}</v-card-title>
+            </v-img>
+
+            <v-card-subtitle class="pt-4">
+              <div>size: height:{{ item.height }} width:{{ item.width }}</div>
+            </v-card-subtitle>
+
+            <v-card-text>
+              {{ item.alt_description }}
+
+              <div>{{ item.download_url }}</div>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn color="primary"> Share </v-btn>
+
+              <v-btn color="primary"> Explore </v-btn>
+            </v-card-actions>
+          </v-card>
+        </template>
+      </Waterfall> -->
     </v-card>
   </div>
 </template>
