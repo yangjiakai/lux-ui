b382a0158e022d57765079f5d43868d73dadd1d3
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-03-15 09:47:12 +0900

    FAQ页面更新

--- src/views/landing/faq/FaqPage.vue
+++ src/views/landing/faq/FaqPage.vue
@@ -21,22 +21,22 @@ const search = ref("");
       <h3 class="text-h5 font-weight-bold">
         FAQ
         <v-chip size="small" class="ma-2" color="primary" text-color="white">
-          4 Components
+          1 Components
         </v-chip>
       </h3>
     </div>
     <v-spacer></v-spacer>
     <v-btn icon>
-      <v-icon>mdi-magnify</v-icon>
+      <v-icon>mdi-home</v-icon>
     </v-btn>
     <v-btn icon>
       <v-icon>mdi-dots-vertical</v-icon>
     </v-btn>
   </v-toolbar>
   <FAQ1 />
-  <FAQ2 />
+  <!-- <FAQ2 />
   <FAQ3 />
-  <FAQ4 />
+  <FAQ4 /> -->
 </template>
 
 <style lang="scss"></style>
