512272f3d59b127d2650c0eb798f5e9d63130eff
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-03-16 16:15:59 +0900

    Feature2 Complete

--- src/views/landing/feature/components/Feature2.vue
+++ src/views/landing/feature/components/Feature2.vue
@@ -1,54 +1,61 @@
 <!--
-* @Component: 
+* @Component:
 * @Maintainer: J.K. Yang
-* @Description: 
+* @Description:
 -->
 <script setup lang="ts"></script>
 
 <template>
   <v-toolbar color="#F9FAFB" height="60">
     <v-toolbar-title class="text-h6 font-weight-bold">
-      <!-- <v-icon class="mr-2">mdi-account</v-icon> -->
-      <span>Hero1</span>
+      <span>Feature2</span>
     </v-toolbar-title>
   </v-toolbar>
-
-  <v-sheet
-    elevation="0"
-    class="mx-auto landing-warpper"
-    color="#F2F5F8"
-    rounded
-  >
-    <v-container class="text-center">
-      <v-sheet color="transparent" elevation="0">
-        <img class="mx-auto" width="200" src="@/assets/logo2.svg" alt="" />
-        <v-card
-          color="transparent"
-          elevation="0"
-          max-width="800"
-          class="mx-auto my-10 py-0py-md-12"
-        >
-          <h1
-            style="color: #4a4d6d"
-            class="font-weight-black text-h3 text-lg-h2 text-xl-h1"
+  <v-sheet elevation="0" class="mx-auto landing-warpper text-left" rounded>
+    <v-sheet
+      elevation="0"
+      color="transparent"
+      max-width="1600 "
+      class="mx-auto my-10"
+    >
+      <v-row no-gutters align="stretch">
+        <v-col cols="12" md="6">
+          <v-card
+            elevation="0"
+            height="100%"
+            class="mx-auto pa-10 pa-md-15 d-flex flex-column justify-center"
           >
-            We organize chaotic
-            <span class="text-primary">internet</span>
-          </h1>
-          <h2 class="text-h6 text-secondary mt-4 mx-auto">
-            Stack is a Spatial Browser for Mindful Online Living
-          </h2>
-        </v-card>
-        <div>
-          <v-btn width="200" height="60" class="text-white mr-5" color="primary"
-            >Get Stack</v-btn
-          >
-          <v-btn variant="outlined" width="200" height="60" class="text-primary"
-            >Watch Demo</v-btn
-          >
-        </div>
-      </v-sheet>
-    </v-container>
+            <div>
+              <RouterLink to="/" class="text-primary font-weight-bold"
+                >WORK WITH US</RouterLink
+              >
+              <h1 class="font-weight-black text-h3 mt-5 mb-10">
+                Get your startup ready for
+                <span class="text-primary">Business</span>
+              </h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+                impedit error labore doloremque fugit! Dolor fugit molestiae
+                vero quos quisquam nobis, eos debitis magni omnis ea incidunt
+                amet voluptate dignissimos!
+              </p>
+              <v-btn variant="outlined" size="large" class="text-white mt-10"
+                >Get In Touch</v-btn
+              >
+            </div>
+          </v-card>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-card class="cover-bg-card1" height="100%">
+            <v-img
+              height="100%"
+              cover
+              src="https://images.unsplash.com/photo-1561909700-7e3abc080cf9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+            ></v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-sheet>
   </v-sheet>
 </template>
 
