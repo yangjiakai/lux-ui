e2b2e42d259c4764771f4d88274b06b94f20c28b
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-03-16 14:50:37 +0900

    Feature Base Create

--- src/views/landing/feature/featurePage.vue
+++ src/views/landing/feature/featurePage.vue
@@ -3,10 +3,42 @@
 * @Maintainer: J.K. Yang
 * @Description: 
 -->
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Feature1 from "./components/Feature1.vue";
+import Feature2 from "./components/Feature2.vue";
+import Feature3 from "./components/Feature3.vue";
+import Feature4 from "./components/Feature4.vue";
+const mode = ref("");
+const search = ref("");
+</script>
 
 <template>
-  <div class=""></div>
+  <!-- ---------------------------------------------- -->
+  <!---Top Toolbar -->
+  <!-- ---------------------------------------------- -->
+  <v-toolbar height="100" color="white">
+    <div class="ml-5">
+      <h3 class="text-h5 font-weight-bold">
+        Feature
+        <v-chip size="small" class="ma-2" color="primary" text-color="white">
+          4 Components
+        </v-chip>
+      </h3>
+    </div>
+    <v-spacer></v-spacer>
+    <v-btn icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon>mdi-dots-vertical</v-icon>
+    </v-btn>
+  </v-toolbar>
+  <div>
+    <Feature1 />
+    <Feature2 />
+    <Feature3 />
+    <Feature4 />
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style lang="scss"></style>
