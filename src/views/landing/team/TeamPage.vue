ad628786b52d73dd008bad55bc0017363ae1be50
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-03-17 15:04:05 +0900

    Team1 Complete

--- src/views/landing/team/TeamPage.vue
+++ src/views/landing/team/TeamPage.vue
@@ -8,8 +8,6 @@ import Team1 from "./components/Team1.vue";
 import Team2 from "./components/Team2.vue";
 import Team3 from "./components/Team3.vue";
 import Team4 from "./components/Team4.vue";
-const mode = ref("");
-const search = ref("");
 </script>
 
 <template>
@@ -17,13 +15,14 @@ const search = ref("");
   <!---Top Toolbar -->
   <!-- ---------------------------------------------- -->
   <v-toolbar height="100" color="white">
-    <v-toolbar-title class="text-h5 font-weight-bold">
-      <!-- <v-icon class="mr-2">mdi-account</v-icon> -->
-      <span>Team</span>
-      <v-chip class="ma-2" color="primary" text-color="white">
-        9 Components
-      </v-chip>
-    </v-toolbar-title>
+    <div class="ml-5">
+      <h3 class="text-h5 font-weight-bold">
+        Team
+        <v-chip size="small" class="ma-2" color="primary" text-color="white">
+          4 Components
+        </v-chip>
+      </h3>
+    </div>
     <v-spacer></v-spacer>
     <v-btn icon>
       <v-icon>mdi-magnify</v-icon>
@@ -32,10 +31,12 @@ const search = ref("");
       <v-icon>mdi-dots-vertical</v-icon>
     </v-btn>
   </v-toolbar>
-  <Team1 />
-  <Team2 />
-  <Team3 />
-  <Team4 />
+  <div>
+    <Team1 />
+    <Team2 />
+    <Team3 />
+    <Team4 />
+  </div>
 </template>
 
 <style lang="scss"></style>
