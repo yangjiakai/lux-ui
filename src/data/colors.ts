8096e36a8e2115545ce72362381c003b89d03b3a
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-03-23 23:48:46 +0900

    colos update

---
+++ src/data/colors.ts
@@ -0,0 +1,721 @@
+// // exports.default = void 0;
+var red = Object.freeze({
+  base: "#f44336",
+  lighten5: "#ffebee",
+  lighten4: "#ffcdd2",
+  lighten3: "#ef9a9a",
+  lighten2: "#e57373",
+  lighten1: "#ef5350",
+  darken1: "#e53935",
+  darken2: "#d32f2f",
+  darken3: "#c62828",
+  darken4: "#b71c1c",
+  accent1: "#ff8a80",
+  accent2: "#ff5252",
+  accent3: "#ff1744",
+  accent4: "#d50000",
+});
+var pink = Object.freeze({
+  base: "#e91e63",
+  lighten5: "#fce4ec",
+  lighten4: "#f8bbd0",
+  lighten3: "#f48fb1",
+  lighten2: "#f06292",
+  lighten1: "#ec407a",
+  darken1: "#d81b60",
+  darken2: "#c2185b",
+  darken3: "#ad1457",
+  darken4: "#880e4f",
+  accent1: "#ff80ab",
+  accent2: "#ff4081",
+  accent3: "#f50057",
+  accent4: "#c51162",
+});
+var purple = Object.freeze({
+  base: "#9c27b0",
+  lighten5: "#f3e5f5",
+  lighten4: "#e1bee7",
+  lighten3: "#ce93d8",
+  lighten2: "#ba68c8",
+  lighten1: "#ab47bc",
+  darken1: "#8e24aa",
+  darken2: "#7b1fa2",
+  darken3: "#6a1b9a",
+  darken4: "#4a148c",
+  accent1: "#ea80fc",
+  accent2: "#e040fb",
+  accent3: "#d500f9",
+  accent4: "#aa00ff",
+});
+var deepPurple = Object.freeze({
+  base: "#673ab7",
+  lighten5: "#ede7f6",
+  lighten4: "#d1c4e9",
+  lighten3: "#b39ddb",
+  lighten2: "#9575cd",
+  lighten1: "#7e57c2",
+  darken1: "#5e35b1",
+  darken2: "#512da8",
+  darken3: "#4527a0",
+  darken4: "#311b92",
+  accent1: "#b388ff",
+  accent2: "#7c4dff",
+  accent3: "#651fff",
+  accent4: "#6200ea",
+});
+var indigo = Object.freeze({
+  base: "#3f51b5",
+  lighten5: "#e8eaf6",
+  lighten4: "#c5cae9",
+  lighten3: "#9fa8da",
+  lighten2: "#7986cb",
+  lighten1: "#5c6bc0",
+  darken1: "#3949ab",
+  darken2: "#303f9f",
+  darken3: "#283593",
+  darken4: "#1a237e",
+  accent1: "#8c9eff",
+  accent2: "#536dfe",
+  accent3: "#3d5afe",
+  accent4: "#304ffe",
+});
+var blue = Object.freeze({
+  base: "#2196f3",
+  lighten5: "#e3f2fd",
+  lighten4: "#bbdefb",
+  lighten3: "#90caf9",
+  lighten2: "#64b5f6",
+  lighten1: "#42a5f5",
+  darken1: "#1e88e5",
+  darken2: "#1976d2",
+  darken3: "#1565c0",
+  darken4: "#0d47a1",
+  accent1: "#82b1ff",
+  accent2: "#448aff",
+  accent3: "#2979ff",
+  accent4: "#2962ff",
+});
+var lightBlue = Object.freeze({
+  base: "#03a9f4",
+  lighten5: "#e1f5fe",
+  lighten4: "#b3e5fc",
+  lighten3: "#81d4fa",
+  lighten2: "#4fc3f7",
+  lighten1: "#29b6f6",
+  darken1: "#039be5",
+  darken2: "#0288d1",
+  darken3: "#0277bd",
+  darken4: "#01579b",
+  accent1: "#80d8ff",
+  accent2: "#40c4ff",
+  accent3: "#00b0ff",
+  accent4: "#0091ea",
+});
+var cyan = Object.freeze({
+  base: "#00bcd4",
+  lighten5: "#e0f7fa",
+  lighten4: "#b2ebf2",
+  lighten3: "#80deea",
+  lighten2: "#4dd0e1",
+  lighten1: "#26c6da",
+  darken1: "#00acc1",
+  darken2: "#0097a7",
+  darken3: "#00838f",
+  darken4: "#006064",
+  accent1: "#84ffff",
+  accent2: "#18ffff",
+  accent3: "#00e5ff",
+  accent4: "#00b8d4",
+});
+var teal = Object.freeze({
+  base: "#009688",
+  lighten5: "#e0f2f1",
+  lighten4: "#b2dfdb",
+  lighten3: "#80cbc4",
+  lighten2: "#4db6ac",
+  lighten1: "#26a69a",
+  darken1: "#00897b",
+  darken2: "#00796b",
+  darken3: "#00695c",
+  darken4: "#004d40",
+  accent1: "#a7ffeb",
+  accent2: "#64ffda",
+  accent3: "#1de9b6",
+  accent4: "#00bfa5",
+});
+var green = Object.freeze({
+  base: "#4caf50",
+  lighten5: "#e8f5e9",
+  lighten4: "#c8e6c9",
+  lighten3: "#a5d6a7",
+  lighten2: "#81c784",
+  lighten1: "#66bb6a",
+  darken1: "#43a047",
+  darken2: "#388e3c",
+  darken3: "#2e7d32",
+  darken4: "#1b5e20",
+  accent1: "#b9f6ca",
+  accent2: "#69f0ae",
+  accent3: "#00e676",
+  accent4: "#00c853",
+});
+var lightGreen = Object.freeze({
+  base: "#8bc34a",
+  lighten5: "#f1f8e9",
+  lighten4: "#dcedc8",
+  lighten3: "#c5e1a5",
+  lighten2: "#aed581",
+  lighten1: "#9ccc65",
+  darken1: "#7cb342",
+  darken2: "#689f38",
+  darken3: "#558b2f",
+  darken4: "#33691e",
+  accent1: "#ccff90",
+  accent2: "#b2ff59",
+  accent3: "#76ff03",
+  accent4: "#64dd17",
+});
+var lime = Object.freeze({
+  base: "#cddc39",
+  lighten5: "#f9fbe7",
+  lighten4: "#f0f4c3",
+  lighten3: "#e6ee9c",
+  lighten2: "#dce775",
+  lighten1: "#d4e157",
+  darken1: "#c0ca33",
+  darken2: "#afb42b",
+  darken3: "#9e9d24",
+  darken4: "#827717",
+  accent1: "#f4ff81",
+  accent2: "#eeff41",
+  accent3: "#c6ff00",
+  accent4: "#aeea00",
+});
+var yellow = Object.freeze({
+  base: "#ffeb3b",
+  lighten5: "#fffde7",
+  lighten4: "#fff9c4",
+  lighten3: "#fff59d",
+  lighten2: "#fff176",
+  lighten1: "#ffee58",
+  darken1: "#fdd835",
+  darken2: "#fbc02d",
+  darken3: "#f9a825",
+  darken4: "#f57f17",
+  accent1: "#ffff8d",
+  accent2: "#ffff00",
+  accent3: "#ffea00",
+  accent4: "#ffd600",
+});
+var amber = Object.freeze({
+  base: "#ffc107",
+  lighten5: "#fff8e1",
+  lighten4: "#ffecb3",
+  lighten3: "#ffe082",
+  lighten2: "#ffd54f",
+  lighten1: "#ffca28",
+  darken1: "#ffb300",
+  darken2: "#ffa000",
+  darken3: "#ff8f00",
+  darken4: "#ff6f00",
+  accent1: "#ffe57f",
+  accent2: "#ffd740",
+  accent3: "#ffc400",
+  accent4: "#ffab00",
+});
+var orange = Object.freeze({
+  base: "#ff9800",
+  lighten5: "#fff3e0",
+  lighten4: "#ffe0b2",
+  lighten3: "#ffcc80",
+  lighten2: "#ffb74d",
+  lighten1: "#ffa726",
+  darken1: "#fb8c00",
+  darken2: "#f57c00",
+  darken3: "#ef6c00",
+  darken4: "#e65100",
+  accent1: "#ffd180",
+  accent2: "#ffab40",
+  accent3: "#ff9100",
+  accent4: "#ff6d00",
+});
+var deepOrange = Object.freeze({
+  base: "#ff5722",
+  lighten5: "#fbe9e7",
+  lighten4: "#ffccbc",
+  lighten3: "#ffab91",
+  lighten2: "#ff8a65",
+  lighten1: "#ff7043",
+  darken1: "#f4511e",
+  darken2: "#e64a19",
+  darken3: "#d84315",
+  darken4: "#bf360c",
+  accent1: "#ff9e80",
+  accent2: "#ff6e40",
+  accent3: "#ff3d00",
+  accent4: "#dd2c00",
+});
+var brown = Object.freeze({
+  base: "#795548",
+  lighten5: "#efebe9",
+  lighten4: "#d7ccc8",
+  lighten3: "#bcaaa4",
+  lighten2: "#a1887f",
+  lighten1: "#8d6e63",
+  darken1: "#6d4c41",
+  darken2: "#5d4037",
+  darken3: "#4e342e",
+  darken4: "#3e2723",
+});
+var blueGrey = Object.freeze({
+  base: "#607d8b",
+  lighten5: "#eceff1",
+  lighten4: "#cfd8dc",
+  lighten3: "#b0bec5",
+  lighten2: "#90a4ae",
+  lighten1: "#78909c",
+  darken1: "#546e7a",
+  darken2: "#455a64",
+  darken3: "#37474f",
+  darken4: "#263238",
+});
+var grey = Object.freeze({
+  base: "#9e9e9e",
+  lighten5: "#fafafa",
+  lighten4: "#f5f5f5",
+  lighten3: "#eeeeee",
+  lighten2: "#e0e0e0",
+  lighten1: "#bdbdbd",
+  darken1: "#757575",
+  darken2: "#616161",
+  darken3: "#424242",
+  darken4: "#212121",
+});
+var shades = Object.freeze({
+  black: "#000000",
+  white: "#ffffff",
+  transparent: "transparent",
+});
+
+// export const materialColorThemes = [
+//   {
+//     key: "red",
+//     colors: [
+//       { key: "lighten-5", color: "#FFEBEE" },
+//       { key: "lighten-4", color: "#FFCDD2" },
+//       { key: "lighten-3", color: "#EF9A9A" },
+//       { key: "lighten-2", color: "#E57373" },
+//       { key: "lighten-1", color: "#EF5350" },
+//       { key: "base", color: "#F44336" },
+//       { key: "darken-1", color: "#E53935" },
+//       { key: "darken-2", color: "#D32F2F" },
+//       { key: "darken-3", color: "#C62828" },
+//       { key: "darken-4", color: "#B71C1C" },
+//       { key: "accent-1", color: "#FF8A80" },
+//       { key: "accent-2", color: "#FF5252" },
+//       { key: "accent-3", color: "#FF1744" },
+//       { key: "accent-4", color: "#D50000" },
+//     ],
+//   },
+//   {
+//     key: "pink",
+//     colors: [
+//       {
+//         key: "lighten-5",
+//         color: "#FCE4EC",
+//       },
+//       {
+//         key: "lighten-4",
+//         color: "#F8BBD0",
+//       },
+//       {
+//         key: "lighten-3",
+//         color: "#F48FB1",
+//       },
+//       {
+//         key: "lighten-2",
+//         color: "#F06292",
+//       },
+//       {
+//         key: "lighten-1",
+//         color: "#EC407A",
+//       },
+//       {
+//         key: "base",
+//         color: "#E91E63",
+//       },
+//       {
+//         key: "darken-1",
+//         color: "#D81B60",
+//       },
+//       {
+//         key: "darken-2",
+//         color: "#C2185B",
+//       },
+//       {
+//         key: "darken-3",
+//         color: "#AD1457",
+//       },
+//       {
+//         key: "darken-4",
+//         color: "#880E4F",
+//       },
+//       {
+//         key: "accent-1",
+//         color: "#FF80AB",
+//       },
+//       {
+//         key: "accent-2",
+//         color: "#FF4081",
+//       },
+//       {
+//         key: "accent-3",
+//         color: "#F50057",
+//       },
+//       {
+//         key: "accent-4",
+//         color: "#C51162",
+//       },
+//     ],
+//   },
+
+//   // purple
+//   {
+//     key: "purple",
+//     colors: [
+//       {
+//         key: "lighten-5",
+//         color: "#F3E5F5",
+//       },
+//       {
+//         key: "lighten-4",
+//         color: "#E1BEE7",
+//       },
+//       {
+//         key: "lighten-3",
+//         color: "#CE93D8",
+//       },
+//       {
+//         key: "lighten-2",
+//         color: "#BA68C8",
+//       },
+//       {
+//         key: "lighten-1",
+//         color: "#AB47BC",
+//       },
+//       {
+//         key: "base",
+//         color: "#9C27B0",
+//       },
+//       {
+//         key: "darken-1",
+//         color: "#8E24AA",
+//       },
+//       {
+//         key: "darken-2",
+//         color: "#7B1FA2",
+//       },
+//       {
+//         key: "darken-3",
+//         color: "#6A1B9A",
+//       },
+//       {
+//         key: "darken-4",
+//         color: "#4A148C",
+//       },
+//       {
+//         key: "accent-1",
+//         color: "#EA80FC",
+//       },
+//       {
+//         key: "accent-2",
+//         color: "#E040FB",
+//       },
+//       {
+//         key: "accent-3",
+//         color: "#D500F9",
+//       },
+//       {
+//         key: "accent-4",
+//         color: "#AA00FF",
+//       },
+//     ],
+//   },
+//   // deepPurple
+//   {
+//     key: "deepPurple",
+//     colors: [
+//       {
+//         key: "lighten-5",
+//         color: "#EDE7F6",
+//       },
+//       {
+//         key: "lighten-4",
+//         color: "#D1C4E9",
+//       },
+//       {
+//         key: "lighten-3",
+//         color: "#B39DDB",
+//       },
+//       {
+//         key: "lighten-2",
+//         color: "#9575CD",
+//       },
+//       {
+//         key: "lighten-1",
+//         color: "#7E57C2",
+//       },
+//       {
+//         key: "base",
+//         color: "#673AB7",
+//       },
+//       {
+//         key: "darken-1",
+//         color: "#5E35B1",
+//       },
+//       {
+//         key: "darken-2",
+//         color: "#512DA8",
+//       },
+//       {
+//         key: "darken-3",
+//         color: "#4527A0",
+//       },
+//       {
+//         key: "darken-4",
+//         color: "#311B92",
+//       },
+//       {
+//         key: "accent-1",
+//         color: "#B388FF",
+//       },
+//       {
+//         key: "accent-2",
+//         color: "#7C4DFF",
+//       },
+//       {
+//         key: "accent-3",
+//         color: "#651FFF",
+//       },
+//       {
+//         key: "accent-4",
+//         color: "#6200EA",
+//       },
+//     ],
+//   },
+//   // blueGrey
+//   {
+//     key: "blueGrey",
+//     colors: [
+//       {
+//         key: "lighten-5",
+//         color: "#ECEFF1",
+//       },
+//       {
+//         key: "lighten-4",
+//         color: "#CFD8DC",
+//       },
+//       {
+//         key: "lighten-3",
+//         color: "#B0BEC5",
+//       },
+//       {
+//         key: "lighten-2",
+//         color: "#90A4AE",
+//       },
+//       {
+//         key: "lighten-1",
+//         color: "#78909C",
+//       },
+//       {
+//         key: "base",
+//         color: "#607D8B",
+//       },
+//       {
+//         key: "darken-1",
+//         color: "#546E7A",
+//       },
+//       {
+//         key: "darken-2",
+//         color: "#455A64",
+//       },
+//       {
+//         key: "darken-3",
+//         color: "#37474F",
+//       },
+//       {
+//         key: "darken-4",
+//         color: "#263238",
+//       },
+//     ],
+//   },
+// ];
+
+export const colorList = [
+  {
+    key: "red",
+    colors: [
+      {
+        key: "base",
+        color: "#f44336",
+      },
+      {
+        key: "lighten5",
+        color: "#ffebee",
+      },
+      {
+        key: "lighten4",
+        color: "#ffcdd2",
+      },
+      {
+        key: "lighten3",
+        color: "#ef9a9a",
+      },
+      {
+        key: "lighten2",
+        color: "#e57373",
+      },
+      {
+        key: "lighten1",
+        color: "#ef5350",
+      },
+      {
+        key: "darken1",
+        color: "#e53935",
+      },
+      {
+        key: "darken2",
+        color: "#d32f2f",
+      },
+      {
+        key: "darken3",
+        color: "#c62828",
+      },
+      {
+        key: "darken4",
+        color: "#b71c1c",
+      },
+      {
+        key: "accent1",
+        color: "#ff8a80",
+      },
+      {
+        key: "accent2",
+        color: "#ff5252",
+      },
+      {
+        key: "accent3",
+        color: "#ff1744",
+      },
+      {
+        key: "accent4",
+        color: "#d50000",
+      },
+    ],
+  },
+  {
+    key: "pink",
+    colors: [
+      { key: "base", color: "#e91e63" },
+      { key: "lighten5", color: "#fce4ec" },
+      { key: "lighten4", color: "#f8bbd0" },
+      { key: "lighten3", color: "#f48fb1" },
+      { key: "lighten2", color: "#f06292" },
+      { key: "lighten1", color: "#ec407a" },
+      { key: "darken1", color: "#d81b60" },
+      { key: "darken2", color: "#c2185b" },
+      { key: "darken3", color: "#ad1457" },
+      { key: "darken4", color: "#880e4f" },
+      { key: "accent1", color: "#ff80ab" },
+      { key: "accent2", color: "#ff4081" },
+      { key: "accent3", color: "#f50057" },
+      { key: "accent4", color: "#c51162" },
+    ],
+  },
+  // purple
+  {
+    key: "purple",
+    colors: [
+      { key: "base", color: "#9c27b0" },
+      { key: "lighten5", color: "#f3e5f5" },
+      { key: "lighten4", color: "#e1bee7" },
+      { key: "lighten3", color: "#ce93d8" },
+      { key: "lighten2", color: "#ba68c8" },
+      { key: "lighten1", color: "#ab47bc" },
+      { key: "darken1", color: "#8e24aa" },
+      { key: "darken2", color: "#7b1fa2" },
+      { key: "darken3", color: "#6a1b9a" },
+      { key: "darken4", color: "#4a148c" },
+      { key: "accent1", color: "#ea80fc" },
+      { key: "accent2", color: "#e040fb" },
+      { key: "accent3", color: "#d500f9" },
+      { key: "accent4", color: "#aa00ff" },
+    ],
+  },
+];
+
+const keys = [
+  "red",
+  "pink",
+  "purple",
+  "deepPurple",
+  "indigo",
+  "blue",
+  "lightBlue",
+  "cyan",
+  "teal",
+  "green",
+  "lightGreen",
+  "lime",
+  "yellow",
+  "amber",
+  "orange",
+  "deepOrange",
+  "brown",
+  "blueGrey",
+  "grey",
+  "shades",
+];
+
+function convertColorObject(colorName, colorObj) {
+  const colorsArray = Object.entries(colorObj).map(([key, color]) => {
+    return { key, color };
+  });
+
+  return {
+    key: colorName,
+    colors: colorsArray,
+  };
+}
+
+export const materialColorThemes = Object.freeze({
+  red: red,
+  pink: pink,
+  purple: purple,
+  deepPurple: deepPurple,
+  indigo: indigo,
+  blue: blue,
+  lightBlue: lightBlue,
+  cyan: cyan,
+  teal: teal,
+  green: green,
+  lightGreen: lightGreen,
+  lime: lime,
+  yellow: yellow,
+  amber: amber,
+  orange: orange,
+  deepOrange: deepOrange,
+  brown: brown,
+  blueGrey: blueGrey,
+  grey: grey,
+  shades: shades,
+});
+
+const colorList2 = [];
+
+convertColorObject("red", red);
+// Object.keys(colors).forEach((key) => {
+//   const color = colors[key];
+//   const colorObj = convertColorObject(key, color);
+//   colorList.push(colorObj);
+// })
--- src/views/ui/ColorsPage.vue
+++ src/views/ui/ColorsPage.vue
@@ -1,151 +1,53 @@
-<template>
-  <v-container>
-    <!-- <Icon class="animate-icon" icon="logos:bamboo" /> -->
-
-    <v-item-group selected-class="select-card">
-      <div class="card-box d-flex flex-wrap">
-        <template v-for="i in 30" :key="i">
-          <v-item v-slot="{ selectedClass, toggle }">
-            <v-card
-              color="#E9E5DC"
-              class="ma-5 shadow-1 card"
-              :class="[selectedClass]"
-              height="330"
-              width="270"
-              @click="toggle"
-            >
-              <v-img
-                height="80%"
-                cover
-                :src="'https://picsum.photos/200/300?random=' + i"
-              >
-                <div class="d-flex">
-                  <v-chip>5</v-chip>
-                  <v-spacer></v-spacer>
-                  <v-avatar size="50" color="red">
-                    <!-- <img src="src" alt="alt"> -->
-                  </v-avatar>
-                </div>
-                <div>
-                  <span class="isLocked">&</span>
-                </div>
-              </v-img>
-              <v-card-actions
-                class="text-h4 font-weight-bold d-flex align-center justify-center"
-              >
-                Lv. {{ i }}
-              </v-card-actions>
-            </v-card>
-          </v-item>
-        </template>
-
-        <!-- <div class="card" v-for="i in 30" :key="i">aa</div> -->
-      </div>
-    </v-item-group>
-    <v-card
-      color="grey"
-      class="d-flex align-center justify-center"
-      height="500"
-    >
-      <v-card class="anime-card shadow-1" color="white"> </v-card>
-    </v-card>
-
-    <v-card class="card-box d-flex flex-wrap" v-if="isFinished">
-      <v-card v-for="image in filteredItems" :key="image.id">
-        <v-img :src="image.urls.small"></v-img>
-        <v-card-title>
-          {{ image.user.username }}
-        </v-card-title>
-      </v-card>
-    </v-card>
-  </v-container>
-</template>
-
+<!--
+* @Component: 
+* @Maintainer: J.K. Yang
+* @Description: 
+-->
 <script setup lang="ts">
-import { Icon, listIcons } from "@iconify/vue";
-
-import { useAxios } from "@vueuse/integrations/useAxios";
-
-const url = ref("https://api.unsplash.com/search/photos?page=1&query=chair");
-const accessKey = ref("mfB0t1DgccWtivNuh8KD06FMIZcun7vE_x_BSYQrfq8");
-
-const { data, isLoading, isFinished, execute } = useAxios(
-  `${url.value}&client_id=${accessKey.value}`
-);
-
-console.log(data.value);
-
-const filteredItems = computed(() => {
-  let result = [];
-  if (isFinished.value) {
-    result = data.value.results;
-    console.log(data.value);
-  }
-  return result;
+import { materialColorThemes } from "@/data/colors";
+const computedColors = computed(() => {
+  const colorKeys = Object.keys(materialColorThemes);
+  const colors = colorKeys.map((key) => {
+    const color = materialColorThemes[key];
+    return {
+      key,
+      color,
+    };
+  });
+  return colors;
 });
 </script>
-<style scoped lang="scss">
-@keyframes rotate {
-  0% {
-    transform: rotate(0deg);
-  }
 
-  100% {
-    transform: rotate(360deg);
-  }
-}
+<template>
+  <div class="">
+    {{ computedColors }}
+    <!-- <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          sm="6"
+          md="4"
+          lg="3"
+          v-for="colorTheme in materialColorThemes"
+        >
+          <v-card>
+            <h1>{{ colorTheme.key }}</h1>
 
-@keyframes breath {
-  0% {
-    border-color: #ccc;
-  }
+            <v-card height="100" v-for="color in colorTheme.colors">
+              <div class="d-flex align-center">
+                <div class="flex-grow-1">
+                  <v-card :color="color.color" height="100" class="mt-5">
+                    <h3>{{ color.key }}</h3>
+                    <h3>{{ color.color }}</h3>
+                  </v-card>
+                </div>
+              </div>
+            </v-card>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container> -->
+  </div>
+</template>
 
-  50% {
-    border-color: #000;
-  }
-  100% {
-    border-color: #ccc;
-  }
-}
-
-@keyframes wave {
-  0% {
-    box-shadow: 0 0 0 0px rgba(255, 255, 255, 1),
-      0 0 0 20px rgba(255, 255, 255, 0.2), 0 0 0 40px rgba(255, 255, 255, 0.6),
-      0 0 0 60px rgba(255, 255, 255, 0.4), 0 0 0 80px rgba(255, 255, 255, 0.2);
-  }
-  100% {
-    box-shadow: 0 0 0 80px rgba(255, 255, 255, 0),
-      0 0 0 60px rgba(255, 255, 255, 0.2), 0 0 0 40px rgba(255, 255, 255, 0.4),
-      0 0 0 20px rgba(255, 255, 255, 0.6), 0 0 0 0px rgba(255, 255, 255, 1);
-  }
-}
-
-.animate-icon {
-  animation: rotate 2s infinite;
-  animation-timing-function: 1s ease-in-out;
-}
-
-.select-card {
-  border: 3px solid #ccc;
-  // animation: breath 3s infinite;
-  animation: wave 1s;
-  animation-timing-function: 1s ease-in-out;
-}
-
-.card {
-  border-radius: 14px;
-}
-
-.anime-card {
-  width: 400px;
-  height: 200px;
-
-  transition: 0.3s ease-in;
-  &:hover {
-    border-radius: 50%;
-    height: 400px;
-    transition: 0.3s ease-out;
-  }
-}
-</style>
+<style scoped lang="scss"></style>
