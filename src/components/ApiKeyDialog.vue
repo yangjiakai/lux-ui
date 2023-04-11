56ad088c76cd1a4653b839efcdf50db39d2afb9d
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-04-11 14:17:23 +0900

    Update ApiKeyDialog.vue

--- src/components/ApiKeyDialog.vue
+++ src/components/ApiKeyDialog.vue
@@ -18,16 +18,21 @@ const key = computed({
     chatStore.saveApiKey(value); // 假设您有一个名为setApiKey的Pinia store mutation
   },
 });
+
+const apiKeyShow = ref(false);
 </script>
 
 <template>
   <v-dialog v-model="chatStore.apiKeyDialog" width="600">
     <v-card>
-      <v-card-title> 请输入您的API KEY </v-card-title>
+      <v-card-title class="font-weight-bold pa-5">
+        Input your Api Key</v-card-title
+      >
       <hr />
       <v-card-text>
         <v-text-field
           color="primary"
+          variant="outlined"
           label="API KEY"
           v-model="key"
           class="px-2 py-1"
@@ -36,12 +41,14 @@ const key = computed({
           autofocus
           clearable
           hide-details
+          :type="apiKeyShow ? 'text' : 'password'"
+          @click:prepend-inner="apiKeyShow = !apiKeyShow"
         ></v-text-field>
       </v-card-text>
 
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn block color="primary" text @click="close">确定</v-btn>
+        <v-btn variant="flat" color="primary" @click="close">OK</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
