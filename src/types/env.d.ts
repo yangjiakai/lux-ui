9780fcafb606fe7d46b2d5ad67ee4f447a44eb97
sun <jaysunxiao@gmail.com>
2023-04-14 15:16:49 +0800

    Add env var base url

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -3,5 +3,6 @@ declare interface ImportMeta {
     // Environment variables defined in .env
     readonly VITE_UNSPLASH_ACCESS_KEY: string;
     readonly VITE_GITHUB_CLIENT_ID: string;
+    readonly VITE_API_BASE_URL: string;
   };
 }
