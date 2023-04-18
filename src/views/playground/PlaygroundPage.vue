162193268cdae8aa6873aff1f0e306c1033b8d59
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-04-18 22:46:27 +0900

    Update Virtual Scroller
    - Use RecycleScroller
    - Add @faker-js/faker

--- src/views/playground/PlaygroundPage.vue
+++ src/views/playground/PlaygroundPage.vue
@@ -8,6 +8,7 @@ import FeatureCard from "./components/FeatureCard.vue";
 import FeatureLoadMore from "./components/FeatureLoadMore.vue";
 import FeatureLotte from "./components/FeatureLotte.vue";
 import FeatureChatBot from "./components/FeatureChatBot.vue";
+import FeatureDynamicScroller from "./components/FeatureDynamicScroller.vue";
 </script>
 
 <template>
@@ -19,6 +20,9 @@ import FeatureChatBot from "./components/FeatureChatBot.vue";
   </FeatureCard>
   <FeatureCard title="ChatBot">
     <FeatureChatBot />
+  </FeatureCard>
+  <FeatureCard title="DynamicScroller">
+    <FeatureDynamicScroller />
   </FeatureCard>
 </template>
 
