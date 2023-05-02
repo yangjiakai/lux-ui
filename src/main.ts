6c88ed2ecdefea8bd5354287d9cc9d9c9982e1b7
godotg <jaysunxiao@gmail.com>
2023-05-02 11:08:28 +0800

    Initial pinia  advance

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,6 @@ import VueApexCharts from "vue3-apexcharts";
 import piniaPersist from "pinia-plugin-persist";
 import PerfectScrollbar from "vue3-perfect-scrollbar";
 import "@/styles/main.scss";
-import router from "./router";
 import i18n from "./plugins/i18n";
 import "vue3-lottie/dist/style.css";
 import Vue3Lottie from "vue3-lottie";
@@ -25,13 +24,16 @@ const pinia = createPinia();
 pinia.use(piniaPersist);
 const app = createApp(App);
 
+app.use(vuetify);
+app.use(pinia);
+app.use(i18n);
+
+import router from "./router";
 app.use(router);
+
 app.use(PerfectScrollbar);
 app.use(MasonryWall);
 app.use(VueVirtualScroller);
 app.use(VueApexCharts);
-app.use(pinia);
-app.use(i18n);
 app.use(Vue3Lottie, { name: "LottieAnimation" });
-app.use(vuetify);
 app.mount("#app");
