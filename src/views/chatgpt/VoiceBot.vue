13905c3ed04234fc1296a842ae55e04e15b279cf
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-05-05 14:05:51 +0900

    Update VoiceBot.vue

--- src/views/chatgpt/VoiceBot.vue
+++ src/views/chatgpt/VoiceBot.vue
@@ -236,7 +236,7 @@ const clearMessages = () => {
 
       <!-- No Message Content -->
       <div class="no-message-container" v-else>
-        <h1 class="text-blue text-h3 text-md-h2 font-weight-bold">
+        <h1 class="text-grey-lighten-2 text-h3 text-md-h2 font-weight-bold">
           Talk With Me
         </h1>
 
