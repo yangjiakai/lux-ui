8017a3a59346c4876b9badf3118b05fa422891d5
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-05-14 21:56:39 +0900

    Update ja.ts

--- src/locales/ja.ts
+++ src/locales/ja.ts
@@ -141,6 +141,9 @@ export default {
     apikey: {
       title: "API Key",
     },
+    chatgptConfig: {
+      title: "ChatGPT Config",
+    },
     chatAssistant: {
       title: "チャットアシスタント",
     },
