b8638d2c698f6c682f637bcbb276850ee074ebc0
jk.yang <yjkbako@gmail.com>
2023-05-31 13:34:37 +0900

    Update ForgotPage.vue

--- src/views/auth/ForgotPage.vue
+++ src/views/auth/ForgotPage.vue
@@ -1,3 +1,23 @@
+<script setup lang="ts">
+
+const email = ref('')
+</script>
 <template>
-  <div>Forgot</div>
+  <div>Forgot
+    <p>
+      <v-row>
+        <v-col cols="12" sm="6" md="4">
+          <v-text-field
+            v-model="email"
+            label="Email"
+            name="email"
+            prepend-icon="mdi-email"
+            type="email"
+            outlined
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </p>
+  </div>
 </template>
