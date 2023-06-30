a3afb2fe734ce88f3b1ab9c2744c363a4242ea8f
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-06-30 22:36:37 +0900

    增加新会话提示内容

--- src/views/playground/components/FeatureFormItem.vue
+++ src/views/playground/components/FeatureFormItem.vue
@@ -9,12 +9,22 @@ import MultiFileUpload from "./formItems/MultiFileUpload.vue";
 
 const price = ref(2000);
 const fileList = ref<any[]>([]);
+const select = ref({ state: 'Florida', abbr: 'FL' })
+const items = ref([
+  { state: 'Florida', abbr: 'FL' },
+  { state: 'Georgia', abbr: 'GA' },
+  { state: 'Nebraska', abbr: 'NE' },
+  { state: 'California', abbr: 'CA' },
+  { state: 'New York', abbr: 'NY' },
+]);
 </script>
 
 <template>
   <div class="">
     <CurrencyItem v-model="price" />
-    <MultiFileUpload v-model="fileList"  />
+    <MultiFileUpload v-model="fileList" />
+    <v-select v-model="select" :hint="`${select.state}, ${select.abbr}`" :items="items" item-title="state"
+      item-value="abbr" label="Select" persistent-hint single-line></v-select>
   </div>
 </template>
 
