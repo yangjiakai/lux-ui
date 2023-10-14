920e6dd0d505e07e26909ce6c2aba39d76852f2e
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-10-14 22:47:38 +0900

    Update ColorsPage.vue

--- src/views/ui/ColorsPage.vue
+++ src/views/ui/ColorsPage.vue
@@ -1,7 +1,7 @@
 <!--
-* @Component: 
+* @Component:
 * @Maintainer: J.K. Yang
-* @Description: 
+* @Description:
 -->
 <script setup lang="ts">
 import { fullColorList } from "@/data/colors";
@@ -9,9 +9,12 @@ import { fullColorList } from "@/data/colors";
 const searchKey = ref("");
 
 const computedColors: any = computed(() => {
-  return fullColorList.filter((colorTheme) => {
+  const fullColors = fullColorList.filter((colorTheme) => {
     return colorTheme.key.toLowerCase().includes(searchKey.value.toLowerCase());
   });
+  console.log(fullColors);
+
+  return fullColors
 });
 </script>
 
@@ -20,42 +23,20 @@ const computedColors: any = computed(() => {
     <!-- ---------------------------------------------- -->
     <!-- Filter Input -->
     <!-- ---------------------------------------------- -->
-    <v-text-field
-      clearable
-      variant="solo"
-      class="elevation-1 ma-3"
-      hide-details
-      prepend-inner-icon="mdi-magnify"
-      append-inner-icon="mdi-palette-outline"
-      placeholder="Search Colors"
-      v-model="searchKey"
-    ></v-text-field>
+    <v-text-field clearable variant="solo" class="elevation-1 ma-3" hide-details prepend-inner-icon="mdi-magnify"
+      append-inner-icon="mdi-palette-outline" placeholder="Search Colors" v-model="searchKey"></v-text-field>
 
     <v-container>
       <v-row>
-        <v-col
-          cols="12"
-          sm="6"
-          md="4"
-          lg="3"
-          v-for="colorTheme in computedColors"
-          :key="colorTheme.key"
-        >
+        <v-col cols="12" sm="6" md="4" lg="3" v-for="colorTheme in computedColors" :key="colorTheme.key">
           <v-card elevation="1" class="mb-10">
             <v-card :color="colorTheme.class" height="200">
               <h1 class="text-h6 font-weight-bold pa-5">
                 {{ colorTheme.key }}
               </h1>
             </v-card>
-            <v-card
-              :color="
-                color.key === 'base' ? colorTheme.class : color.colorClass
-              "
-              height="100"
-              class="mt-5 pa-5"
-              v-for="color in colorTheme.colors"
-              :key="color.key"
-            >
+            <v-card :color="color.key === 'base' ? colorTheme.class : color.colorClass
+              " height="100" class="mt-5 pa-5" v-for="color in colorTheme.colors" :key="color.key">
               <h3>
                 {{ color.key === "base" ? colorTheme.class : color.colorClass }}
               </h3>
